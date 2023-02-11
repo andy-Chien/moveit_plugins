f6d07132fcd433d9051d62c9f4968096f0e00fd6
Andy <N2107687J@e.ntu.edu.sg>
2023-02-11 16:26:21 +0800

    add shapes into scene

--- planning_adapter/src/add_trajectory_obstacles.cpp
+++ planning_adapter/src/add_trajectory_obstacles.cpp
@@ -37,6 +37,8 @@
 #include <thread>
 #include <moveit/planning_request_adapter/planning_request_adapter.h>
 #include <class_loader/class_loader.hpp>
+#include <geometric_shapes/shape_operations.h>
+#include <Eigen/Geometry>
 
 #include "mr_msgs/srv/get_robot_trajectory_obstacle.hpp"
 
@@ -61,6 +63,11 @@ public:
     obs_req->header = req.start_state.joint_state.header;
     obs_req->robot_name = robot_name_;
 
+    const auto world_copied = std::make_shared<
+      collision_detection::World>(*(planning_scene->getWorld()));
+    rclcpp::Time t_cw = this_node_->now();
+
+
     if(!client_->wait_for_service(std::chrono::milliseconds(500))){
       RCLCPP_ERROR(logger,
         "Wait for service '%s' failed!", service_name.c_str());
@@ -71,27 +78,85 @@ public:
 
     if(future.wait_for(std::chrono::milliseconds(500)) != std::future_status::timeout)
     {
+      std::vector<shapes::ShapeConstPtr> shapes;
+      EigenSTL::vector_Isometry3d shape_poses;
+
       for(const auto& obs : future.get()->obstacles_list){
-        if(!addObstacles(obs, planning_scene)){
+        if(!shapesAndPosesFromObstacles(obs, shapes, shape_poses)){
           RCLCPP_ERROR(logger, "Add obstacles failed!");
           return false;
         }
+        const Eigen::Isometry3d& world_to_object_header_transform = 
+          planning_scene->getFrameTransform(obs.header.frame_id);
+        world_copied->addToObject(
+          obs.name, world_to_object_header_transform, shapes, shape_poses);
       }
     }else{
       RCLCPP_ERROR(logger, "Service '%s' time out!", service_name.c_str());
       return false;
     }
+    rclcpp::Time t_cp = this_node_->now();
+    const auto planning_scene_copied = std::make_shared<
+      planning_scene::PlanningScene>(planning_scene->getRobotModel(), world_copied);
+    planning_scene_copied->setCurrentState(planning_scene->getCurrentState());
+
     rclcpp::Time t2 = this_node_->now();
-    RCLCPP_INFO(logger, "xxxxxx Adapt time = %f xxxxxx", (t2-t1).seconds());
-    return planner(planning_scene, req, res);
+    RCLCPP_INFO(logger, "xxxxxx Adapt time = %f, world copy time = %f, scene copy time = %f xxxxxx", 
+      (t2 - t1).seconds(), (t_cw - t1).seconds(), (t2 - t_cp).seconds());
+    return planner(planning_scene_copied, req, res);
   }
 
-  bool addObstacles(const mr_msgs::msg::Obstacles& obs, 
-                    const planning_scene::PlanningSceneConstPtr& scene) const
+  bool shapesAndPosesFromObstacles(const mr_msgs::msg::Obstacles& obs, 
+                    std::vector<shapes::ShapeConstPtr>& shapes,
+                    EigenSTL::vector_Isometry3d& shape_poses) const
   {
     std::cout<<"size = "<<obs.meshes.size()<<", "<<obs.meshes_poses.size()<<", "<<
       obs.primitives.size()<<", "<<obs.primitives_poses.size()<<std::endl;
-    return true;
+
+    const size_t num_shapes = obs.primitives.size() + obs.meshes.size();
+    shapes.clear();
+    shapes.reserve(num_shapes);
+    shape_poses.clear();
+    shape_poses.reserve(num_shapes);
+
+    const auto& pose_msg_to_eigen = [](const mr_msgs::msg::Pose& msg)
+    {
+      Eigen::Translation3d trans(msg.pose[0], msg.pose[1], msg.pose[2]);
+      Eigen::Quaterniond quat(msg.pose[3], msg.pose[4], msg.pose[5], msg.pose[6]);
+      if ((quat.x() == 0) && (quat.y() == 0) && (quat.z() == 0) && (quat.w() == 0)){
+        RCLCPP_WARN(logger, "Empty quaternion found in pose message. Setting to neutral orientation.");
+        quat.setIdentity();
+      }else{
+        quat.normalize();
+      }
+      return Eigen::Isometry3d(trans * quat);
+    };
+
+    auto append = [&shapes, &shape_poses, &pose_msg_to_eigen](
+      shapes::Shape* s, const mr_msgs::msg::Poses& poses_msg){
+      if (!s)
+        return;
+      for(const auto& pose_msg : poses_msg.poses)
+      {
+        shape_poses.emplace_back(std::move(pose_msg_to_eigen(pose_msg)));
+        shapes.emplace_back(shapes::ShapeConstPtr(s));
+      }
+    };
+
+    auto treat_shape_vectors = [&append](const auto& shape_vector,
+      const auto& shape_poses_msg, const char* shape_type){
+      if(shape_vector.size() != shape_poses_msg.size()){
+        RCLCPP_ERROR(logger, "Number of '%s' doesn't match number of it's poses!", shape_type);
+        return false;
+      }
+      for(size_t i=0; i<shape_vector.size(); i++){
+        append(shapes::constructShapeFromMsg(shape_vector[i]), shape_poses_msg[i]);
+      }
+      return true;
+    };
+
+    return (treat_shape_vectors(obs.meshes, obs.meshes_poses, "MESH") &&
+            treat_shape_vectors(obs.primitives, obs.primitives_poses, "PRIMITIVES"));
   }
 
   void initialize(const rclcpp::Node::SharedPtr& node, 
